72a2f23fe610a5abd5c5f381d6c005be64e3e4a0
lggeorgieva <lggeorgieva@gmail.com>
2021-06-14 11:29:16 +0100

    Update main.js
    Identations

--- js/main.js
+++ js/main.js
@@ -1,18 +1,12 @@
 function randomIntFromInterval(min, max) { // min and max included
-if (min < 0 || max < 0) {
-    return 'min и max должны быть больше нуля!';
-  }
-
-
-
-if(min > max){
-  [min, max] = [max, min];
-    //max = [min, max = min][0];
-
-
-}
-  return Math.floor(Math.random() * (max - min + 1) + min);
-
+    if (min < 0 || max < 0) {
+        return 'min и max должны быть больше нуля!';
+    }
+    if (min > max) {
+        [min, max] = [max, min];
+        //max = [min, max = min][0];
+    }
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 console.log(randomIntFromInterval(1, 100));
