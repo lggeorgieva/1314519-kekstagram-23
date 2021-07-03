48d842f991e111f9bd3d74d373586cde7749582d
lggeorgieva <lggeorgieva@gmail.com>
2021-07-04 00:11:56 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let images =[];
+
 function randomIntFromInterval(min, max) { // min and max included
   if (min < 0 || max < 0) {
     return 'min и max должны быть больше нуля!';
@@ -9,6 +11,28 @@ function randomIntFromInterval(min, max) { // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-randomIntFromInterval(1, 100);
-//console.log(randomIntFromInterval(100, 1));
-//console.log(randomIntFromInterval(-100, 1));
+let message =["Всё отлично!", "В целом всё неплохо.", "Но не всё.",
+"Когда вы делаете фотографию, хорошо бы убирать палец из кадра.", " В конце концов это просто непрофессионально.",
+"Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.", "Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.",
+"Лица у людей на фотке перекошены, как будто их избивают.", "Как можно было поймать такой неудачный момент?!"];
+
+let authors= ['Lilia', 'Patrick', 'Vivi', 'Olga', 'Daniel']
+
+for(let i =1; i<= 25; i++){
+  image = {
+    id: i,
+    url: 'photos/'+ i + '.jpg',
+    description: 'Lovely lake',
+    likes: randomIntFromInterval(15, 200),
+    comments: message[randomIntFromInterval(0, message.length-1)],
+    name: authors[randomIntFromInterval(0, authors.length-1)]
+
+  }
+
+  images.push(image);
+}
+
+
+
+
+console.log(images);
