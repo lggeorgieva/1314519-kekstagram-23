123891456bc8ac32ce1ab54a94b0cf2069d41639
lggeorgieva <lggeorgieva@gmail.com>
2021-07-04 07:22:13 +0100

    Update main.js
    Indent

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const images =[];
+let images =[];
 
 function randomIntFromInterval(min, max) { // min and max included
   if (min < 0 || max < 0) {
@@ -11,30 +11,25 @@ function randomIntFromInterval(min, max) { // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-  const message =['Всё отлично!', 'В целом всё неплохо.', 'Но не всё.',
-'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.',
-'В конце концов это просто непрофессионально.',
-'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-'Лица у людей на фотке перекошены, как будто их избивают.", "Как можно было поймать такой неудачный момент?!'];
+const message =['Всё отлично!', 'В целом всё неплохо.', 'Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.',
+  'В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают.", "Как можно было поймать такой неудачный момент?!'];
 
-  const authors= ['Lilia', 'Patrick', 'Vivi', 'Olga', 'Daniel'];
+const authors= ['Lilia', 'Patrick', 'Vivi', 'Olga', 'Daniel'];
 
-  for(let i =1; i<= 25; i++){
-  let image = {
-    id: i,
-    url: 'photos/'+ i + '.jpg',
+for(let idx =1; idx<= 25; idx++){
+  const image = {
+    id: idx,
+    url: 'photos/'+ idx + '.jpg',
     description: 'Lovely lake',
     likes: randomIntFromInterval(15, 200),
     comments: message[randomIntFromInterval(0, message.length-1)],
     name: authors[randomIntFromInterval(0, authors.length-1)]
-
   }
-
   images.push(image);
 }
 
-
-
-
-  console.log(images);
+console.log(images);
