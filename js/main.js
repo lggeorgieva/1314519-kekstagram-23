34956aa62afe65018229507a913c52f83349dc2e
lggeorgieva <lggeorgieva@gmail.com>
2021-07-14 11:37:26 +0100

    Upload and Scaling

--- js/main.js
+++ js/main.js
@@ -1,14 +1,74 @@
-function randomIntFromInterval(min, max) { // min and max included
-  if (min < 0 || max < 0) {
-    return 'min и max должны быть больше нуля!';
-  }
-  if (min > max) {
-    [min, max] = [max, min];
-    //max = [min, max = min][0];
-  }
-  return Math.floor(Math.random() * (max - min + 1) + min);
+
+//alert("Hello!");
+//document.getElementById('ImageOverDone').style.display= 'block';
+//document.querySelector('.img-upload__overlay').style.display= 'block';
+
+function loadImage() {
+//  const preview = document.querySelector('img');
+  const file = document.querySelector('input[type=file]').files[0];
+  //alert(file.name);
+  //document.querySelector('.img-upload__overlay').style.display= 'block';
+  document.getElementById('ImageSelected').src='img/' + file.name;
+  //document.getElementById('ImageSelected').style='transform: scale(0.75)';
+  let overlay = document.querySelector('.img-upload__overlay');
+  overlay.classList.remove('hidden');
+  //overlay.classList.add('modal-open');
+  document.body.classList.add('modal-open');
+  //const reader = new FileReader();
+
+  //reader.addEventListener("load", function () {
+    // convert image file to base64 string
+    //preview.src = reader.result;
+  //}, false);
+
+  //if (file) {
+  //  reader.readAsDataURL(file);
+//  }
 }
 
-randomIntFromInterval(1, 100);
-//console.log(randomIntFromInterval(100, 1));
-//console.log(randomIntFromInterval(-100, 1));
+
+function closeImage() {
+  document.body.classList.remove('modal-open');
+  let overlay = document.querySelector('.img-upload__overlay');
+  overlay.classList.add('hidden');
+  //document.querySelector('.img-upload__overlay').style.display= 'none';
+}
+
+
+let scaleFactor = 100;
+
+function reduceScaleBy25() {
+  if (scaleFactor >= 50) {
+    scaleFactor -= 25;
+  }
+  document.querySelector('.scale__control--value').value=scaleFactor + '%';
+  document.getElementById('ImageSelected').style='transform: scale(' + scaleFactor/100 + ')' ;
+}
+
+function increaseScaleBy25() {
+  if (scaleFactor <= 75){
+    scaleFactor += 25;
+  }
+  document.querySelector('.scale__control--value').value=scaleFactor + '%';
+  document.getElementById('ImageSelected').style='transform: scale(' + scaleFactor/100 + ')' ;
+}
+
+/*
+const button = document.querySelector('.click-button');
+const popup = document.querySelector('.content');
+
+// Prevent default
+button.onclick = function (evt) {
+  evt.preventDefault();
+
+  // show a class
+  popup.classList.add('show');
+};
+
+nextButton.addEventListener('click', () => {
+  markerGroup.clearLayers();
+  points.slice(points.length / 2).forEach((point) => {
+    createMarker(point);
+  });
+  nextButton.remove();
+});*/
