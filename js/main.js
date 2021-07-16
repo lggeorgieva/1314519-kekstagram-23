7c3176678ad08b3a7ea56c9b5a7abffc5504edac
PatrickMaier <C.Patrick.Maier@gmail.com>
2021-07-16 12:49:28 +0100

    effects applied

--- js/main.js
+++ js/main.js
@@ -1,57 +1,59 @@
+let scaleFactor = 100;  // global variable recording current scale factor
 
-//alert("Hello!");
-//document.getElementById('ImageOverDone').style.display= 'block';
-//document.querySelector('.img-upload__overlay').style.display= 'block';
 
 function loadImage() {
-//  const preview = document.querySelector('img');
+  // get selected image file
   const file = document.querySelector('input[type=file]').files[0];
-  //alert(file.name);
-  //document.querySelector('.img-upload__overlay').style.display= 'block';
-  document.getElementById('ImageSelected').src='img/' + file.name;
-  //document.getElementById('ImageSelected').style='transform: scale(0.75)';
+
+  // reset scale factor to 100%
+  scaleFactor = 100;
+  document.querySelector('.scale__control--value').value = scaleFactor + '%';
+
+  // display image at 100% scale and with no effects
+  let img = document.getElementById('ImageSelected');
+  img.src = 'photos/' + file.name;
+  img.style.transform = 'scale(' + scaleFactor/100 + ')';
+  img.classList = [];
+
+  // populate image to effects previews
+  let previews = document.querySelectorAll('.effects__preview');
+  for (p of previews) {
+    p.style.backgroundImage = 'url(photos/' + file.name + ')';
+  }
+
+  // show overlay
   let overlay = document.querySelector('.img-upload__overlay');
   overlay.classList.remove('hidden');
-  //overlay.classList.add('modal-open');
   document.body.classList.add('modal-open');
-  //const reader = new FileReader();
-
-  //reader.addEventListener("load", function () {
-    // convert image file to base64 string
-    //preview.src = reader.result;
-  //}, false);
-
-  //if (file) {
-  //  reader.readAsDataURL(file);
-//  }
 }
 
 
 function closeImage() {
+  // hide overlay
   document.body.classList.remove('modal-open');
   let overlay = document.querySelector('.img-upload__overlay');
   overlay.classList.add('hidden');
-  //document.querySelector('.img-upload__overlay').style.display= 'none';
 }
 
-
-let scaleFactor = 100;
 
 function reduceScaleBy25() {
-  if (scaleFactor >= 50) {
-    scaleFactor -= 25;
-  }
-  document.querySelector('.scale__control--value').value=scaleFactor + '%';
-  document.getElementById('ImageSelected').style='transform: scale(' + scaleFactor/100 + ')' ;
+  if (scaleFactor >= 50) { scaleFactor -= 25; }
+  document.querySelector('.scale__control--value').value = scaleFactor + '%';
+  document.getElementById('ImageSelected').style.transform = 'scale(' + scaleFactor/100 + ')' ;
 }
 
+
 function increaseScaleBy25() {
-  if (scaleFactor <= 75){
-    scaleFactor += 25;
-  }
-  document.querySelector('.scale__control--value').value=scaleFactor + '%';
-  document.getElementById('ImageSelected').style='transform: scale(' + scaleFactor/100 + ')' ;
+  if (scaleFactor <= 75){ scaleFactor += 25; }
+  document.querySelector('.scale__control--value').value = scaleFactor + '%';
+  document.getElementById('ImageSelected').style.transform = 'scale(' + scaleFactor/100 + ')' ;
 }
+
+
+function setEffect(effect) {
+  document.getElementById('ImageSelected').classList=[effect];
+}
+
 
 /*
 const button = document.querySelector('.click-button');
